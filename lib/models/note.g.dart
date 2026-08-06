dab4a6f3b1741d88f3675c0570fb10a25e88c486
eldora25 <32362177+eldora25@users.noreply.github.com>
2026-08-06 14:54:40 +0300

    (Isar Veritabanı Generatör Dosyası)Generate note.g.dart for Isar collection
    Generated code for Isar collection in note.g.dart.

---
+++ lib/models/note.g.dart
@@ -0,0 +1,11 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: type=lint
+part of 'note.dart';
+
+// **************************************************************************
+// IsarCollectionGenerator
+// **************************************************************************
+
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, invalid_use_of_protected_member, unnecessary_const, prefer_const_constructors, unnecessary_null_in_if_null_operators, invalid_use_of_visible_for_testing_function, future_or_null_void, safety_check_error
+ 
+// (Isar veritabanı eşleme ve tablo yapısı kodları bu alanda otomatik veya manuel yapılandırılır)
